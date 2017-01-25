bb6bba7e7d6c93d2033c73ba9d989f128655b14d
Brix <Brix.angeles@gmail.com>
2017-01-25 15:38:14 -0800

    added key press

--- sketch.js
+++ sketch.js
@@ -11,6 +11,18 @@ function draw() {
   s.show();
 }
 
+funtion keyPressed(){
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 1);
+  }
+}
+
 function Snake(){
   this.x = 0;
   this.y = 0;
