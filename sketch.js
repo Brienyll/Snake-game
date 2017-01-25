d57f2a9161ccae7dc03690b890dc9081242bc458
Brix <Brix.angeles@gmail.com>
2017-01-25 15:51:52 -0800

    added dir function

--- sketch.js
+++ sketch.js
@@ -19,23 +19,6 @@ function keyPressed(){
   } else if (keyCode === RIGHT_ARROW) {
     s.dir(1, 0);
   } else if (keyCode === LEFT_ARROW) {
-    s.dir(-1, 1);
-  }
-}
-
-function Snake(){
-  this.x = 0;
-  this.y = 0;
-  this.yspeed = 0;
-  this.xspeed = 1;
-
-  this.update = function() {
-    this.x = this.x + this.xspeed;
-    this.y = this.y + this.yspeed;
-  }
-
-  this.show = function () {
-    fill(255);
-    rect(this.x, this.y, 10, 10);
+    s.dir(-1, 0);
   }
 }
